8c751fb6d452b55ab9ddf760b6e5e27207496288
Vnswx <dexieskull@gmail.com>
2022-09-30 17:18:51 +0700

    omaga

---
+++ artis.php
@@ -0,0 +1,31 @@
+<?php
+    include "koneksi.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        <?php
+            include "artis.css";
+        ?>
+    </style>
+</head>
+<body>
+<div class="navbar">
+        <div class="logo"><img src="ht.png" alt=""></div>
+        <ul>
+            <li><a href="#">Login</a></li>
+        </ul>
+    </div>  
+    <br>
+
+    <div class="lk"><h1><a href="#">Punk Music</a></h1></div>   
+    <div class="no">
+
+    </div>
+</body>
+</html>
---
+++ musik.php
@@ -0,0 +1,31 @@
+<?php
+    include "koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400&display=swap" rel="stylesheet">
+    <style>
+        <?php
+        include "ojk.css";
+        ?>
+    </style>
+</head>
+<body>
+    <div class="navbar">
+    <div class="logo"><img src="ht.png" alt=""></div>
+        <a href="login.php">Login</a>
+    </div>
+
+    <div class="jls">
+        <h1>TOPS SONG's</h1>
+    </div>
+</body>
+</html>
